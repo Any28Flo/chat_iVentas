18e61bb1a385ff903a6c4a7ba638b7e0cf8a3a41
Analin Flores <foglzerika@gmail.com>
2023-07-22 15:10:31 -0600

    feat | Create schema for Pusher actions

--- chat_server/src/schema/index.ts
+++ chat_server/src/schema/index.ts
@@ -4,8 +4,25 @@ const schema = buildSchema(`
   type Query {
     hello: String
   }
+  type Message {
+    id: ID!
+    content: String!
+    timestamp: String!
+  }
+
+  type Query {
+    messages: [Message!]!
+  }
+
+  type Mutation {
+    sendMessage(content: String!): Message!
+  }
+
+  type Subscription {
+    newMessage: Message!
+  }
 `)
 
 export {
-    schema
+  schema
 }
--- chat_server/src/susbscriptions/messageSubscription.ts
+++ chat_server/src/susbscriptions/messageSubscription.ts
@@ -1,7 +1,6 @@
 // Implement your logic to handle real-time functionality using Pusher
 // For example, listening for new messages and sending them via Pusher
 
-import { Request, Response } from "express";
 import Pusher from 'pusher';
 import config from "../config";
 
@@ -14,15 +13,3 @@ const pusher = new Pusher({
 });
 
 export { pusher }
-
-const postMessage = (req: Request, res: Response) => {
-    const { username, message } = req.body;
-
-    // Trigger a new message event on Pusher
-    pusher.trigger('chat', 'message', {
-        username,
-        message,
-    });
-
-    res.status(200).json({ success: true });
-};
